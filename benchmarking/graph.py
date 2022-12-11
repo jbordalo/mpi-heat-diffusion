91ec141d24f34db2df2f6d6068d56ac17c33ec8c
jbordalo <j.bordalo@campus.fct.unl.pt>
2022-12-11 19:50:37 +0000

    Add graphs

--- benchmarking/graph.py
+++ benchmarking/graph.py
@@ -14,4 +14,30 @@ for file in os.listdir(PATH):
 print(df.head())
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-	print(df.mean().sort_values())
+	# print(df.mean().sort_values())
+	table = df.describe().round(3).T[["mean", "max", "min"]]
+	print(table.to_latex())
+
+mean = df.mean()
+
+titles = [
+	"Initial",
+	"Master-Worker",
+	"Master-Worker with Overlapping Communication"
+]
+
+ranges = [2 ** np.arange(6), 2 ** np.arange(1, 6), 2 ** np.arange(1, 6)]
+
+par = ["parallel" + str(x) for x in ranges[0]]
+mw = ["master-worker" + str(x) for x in ranges[1]]
+mw_async = ["master-worker-async" + str(x) for x in ranges[2]]
+
+for i, dframe in enumerate([par, mw, mw_async]):
+	plt.title(f"Average execution time by number of nodes\n for '{titles[i]}' version")
+	df = mean[dframe]
+	plt.plot(df.index, df, ".k--")
+	plt.xlabel("# Nodes")
+	plt.ylabel("Time / s")
+	plt.xticks(df.index, ranges[i])
+	plt.savefig(titles[i].replace(" ", ""))
+	plt.clf()
